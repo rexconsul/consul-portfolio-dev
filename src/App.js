82624de8baeb2248deac8e62e3fac9c5f4547c6d
RConsul <regin.consul@corelogic.com.au>
2021-12-16 20:59:10 +0800

    Added AboutMe Section

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Navbar from './components/Navbar'
 import Card from './components/Card'
 import Jumbo from './components/Jumbo'
 import Footer from './components/Footer'
+import AboutMe from './components/AboutMe'
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
       <Jumbo/>
+      <AboutMe/>
       <Container>
         <Row>
           <Col><Card/></Col>
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {Jumbotron, Button} from 'react-bootstrap'
+
+function AboutMe() {
+  return (
+    <Jumbotron>
+      <h2>Hi! My name is Rex!</h2>
+      <h5>I'am a Web Developer</h5>
+      <Button variant='primary'>Know More</Button>
+    </Jumbotron>
+  )
+}
+
+export default AboutMe;
